8eb2f57c579a6575c971b1c25e447468683f4725
EANimesha <nimeshadilini999@gmail.com>
2020-05-08 23:33:40 +0530

    implemented get and filtering  task by search  with persistence

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,3 +1,4 @@
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 import { TaskRepository } from './task.repository';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -11,27 +12,10 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository:TaskRepository
   ){}
-  // private tasks: Task[] = [];
 
-  // getAllTasks() {
-  //   return this.tasks;
-  // }
-
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       task =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-
-  //   return tasks;
-  // }
+  async getTasks(filterDto:GetTasksFilterDto):Promise<Task[]>{
+    return this.taskRepository.getTasks(filterDto);
+  }
 
   async getTaskById(id:number):Promise<Task>{
     const found=await this.taskRepository.findOne(id);

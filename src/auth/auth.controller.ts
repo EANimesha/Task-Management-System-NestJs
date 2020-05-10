e174a916541369627411d6f6285967954fa002e8
EANimesha <nimeshadilini999@gmail.com>
2020-05-10 15:38:07 +0530

    added a @getUser() decorator instead of calling whole request

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
+import { User } from './user.entity';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/user-credentials.dto';
 import { Controller, Post, Body, ValidationPipe, UseGuards ,Req} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './get-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -21,7 +23,7 @@ export class AuthController {
 
     @Post('/test')
     @UseGuards(AuthGuard())
-    test(@Req() req){
-        console.log(req)
+    test(@GetUser() user:User){
+        console.log(user);
     }
 }

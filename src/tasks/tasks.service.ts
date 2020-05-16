b252e0496a1700088efeadba7bb5774ecd2c52dd
EANimesha <nimeshadilini999@gmail.com>
2020-05-17 01:06:55 +0530

    authorization- creating tasks for users

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -5,6 +5,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task-dto';
 import { TaskStatus } from './task-status.enum';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -27,8 +28,8 @@ export class TasksService {
     return found;
   }
 
-  async createTask(createTaskDto: CreateTaskDto){
-    return this.taskRepository.createTask(createTaskDto);
+  async createTask(createTaskDto: CreateTaskDto,user:User){
+    return this.taskRepository.createTask(createTaskDto,user);
   }
 
   async deleteTask(id: number): Promise<void> {

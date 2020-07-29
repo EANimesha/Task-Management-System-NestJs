e444b95345e679c8b41420248279db0434f15c6a
EANimesha <nimeshadilini999@gmail.com>
2020-07-30 01:48:38 +0530

    applying configuration codebase

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,6 +6,9 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 
+import * as config from 'config';
+
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy){
     constructor(
@@ -14,7 +17,7 @@ export class JwtStrategy extends PassportStrategy(Strategy){
     ){
         super({
             jwtFromRequest:ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey:'topSecret51'
+            secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret')
         });
     }
 
